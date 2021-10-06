c32b8d6e8762a559bb2fa640faa9652b098d6c50
Ben_B <ben.briscoe01@icloud.com>
2021-10-05 21:54:58 -0500

    Final cleaning touches to code

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 //A creative take on a memory game.
+//When time allows I will change code to handle data management and UI changes in seperate functions. 
 
+
+
+//This prevents reloading of the page for submits
+document.addEventListener('submit',function(e){
+    e.preventDefault
     
-//Global variables used to ensure extra chickens can't be clicked
-let firstSelectedChicken =99
-let secondSelectedChicken =98
-let chickensSelected = 0
-
-
-
+})
 
 //This event listener manages the clicking of buttons to start and continue games as well as looking at the rules
 document.addEventListener('click',function(e){
+    
+    //Defines handles to refer to the three main divs of the html file. These are toggled to be visible or not as needed
     const rules = document.querySelector("#ruleDiv")
     const form = document.querySelector("#formDiv")
     const game = document.querySelector("#gameDiv")
+    
+    //Rule Button Click
     if(e.target.id === "ruleButton"){
         rules.classList.remove("invisible")
         if(form.classList.contains("invisible") === false) {
@@ -26,6 +30,8 @@ document.addEventListener('click',function(e){
         }
 
     }
+
+    //New Game Button Click
     if(e.target.id === "newGameButton"){
         form.classList.remove("invisible")
         if(rules.classList.contains("invisible") === false) {
@@ -36,6 +42,8 @@ document.addEventListener('click',function(e){
             depopulateGameDiv()
         }
     }
+
+    //Button for new game creation form
     if(e.target.id === "startNewGameButton"){
         //If Game is currently going, check to make sure user wants to start a new game.
         if(localStorage.gameInProgress==="true") {
@@ -61,6 +69,7 @@ document.addEventListener('click',function(e){
         }
     }
 
+    //Start Game button that appears if trying to start new game while game is in progress
     if(e.target.id === "sureButton") {
         document.querySelector('#warning').remove()
         document.querySelector('#sureButton').remove()
@@ -71,6 +80,7 @@ document.addEventListener('click',function(e){
         createGame()
     }
 
+    //Button to cancel new game creation that appears if trying to start new game while game is in progress
     if(e.target.id === "cancelButton") {
         clearForm()
         game.classList.remove("invisible")
@@ -80,6 +90,7 @@ document.addEventListener('click',function(e){
         document.querySelector('#cancelButton').remove()
     }
 
+    //Button that brings up the current game in case page has been refreshed or user has navigated away from current game
     if(e.target.id ==="continueGameButton") {
         game.classList.remove("invisible")
         form.classList.add("invisible")
@@ -91,31 +102,61 @@ document.addEventListener('click',function(e){
     }
 })
 
+
+
 //This function runs at the setup a new game
 function createGame() {
+
+    //Declaring some variables we will use to ensure form was filled out correctly.
+    const name = document.querySelector("#userName")
+    let gameType = "invalid"
+    const radioBoxes = document.getElementsByName("gameType")
+    
+    //Changes gameType to valid if one of the boxes was checked
+    for(i=0;i<radioBoxes.length;i++){
+        if(radioBoxes[i].checked === true) {
+                        gameType = "valid"
+        }
+    }
+    //Makes sure user name was entered, if not throws alert
+    if(name.value === "") {
+        throw alert("Please make sure you enter a Player Name")
+    }
+
+    //Makes sure game type was selected, if not throws alert
+    if(gameType==="invalid") {
+        throw alert("Please make sure you have selected how many eggs you would like to collect.")
+    }
+
+    //Stores user name
+    localStorage.setItem("userName",name.value)
+
+    //Gets info from form radio boxes and stores selected game type
+    for(i=0;i<radioBoxes.length;i++){
+        if(radioBoxes[i].checked === true) {
+                        localStorage.setItem("gameType",radioBoxes[i].value)
+        }
+    }
+
+    //Clear the create game form 
+    clearForm()
+    
     //Get rid of form div and pull up game div, let browser know game is started, and clear form
     document.querySelector("#gameDiv").classList.remove("invisible")
     document.querySelector("#formDiv").classList.add("invisible")
     localStorage.setItem("gameInProgress", "true")
-    
-    //Store some data we will use in click events to make sure too many chickens aren't selected
+        
+    //We will use the item 'chickensSelected' in click events to make sure too many chickens aren't selected
     localStorage.setItem('chickensSelected',0)
 
-    //Store game data
-    const name = document.querySelector("#userName")
-    localStorage.setItem("userName",name.value)
-    const types = document.getElementsByName("gameType")
-    for(i=0;i<types.length;i++){
-        if(types[i].checked === true) {
-                        localStorage.setItem("gameType",types[i].value)
-        }
-    }
-    clearForm()
-    setScore()
-    arrangeChickensAndEggs()
+    //The item 'hiddenDivs' is used to manage matching pairs that have previously been found during a game. This step makes sure our newly created game doesnt hide chickens thinking they have already been found.
     if(localStorage.hiddenDivs!=undefined){
         localStorage.removeItem('hiddenDivs')
     }
+
+    //Gets data in local storage for memory game items
+    arrangeChickensAndEggs()
+    //Makes all UI changes
     populateGameDiv()
 }
 
@@ -131,22 +172,18 @@ function clearForm() {
     document.querySelector('#userName').value=""
 }
 
-function shuffleArray(array) {
-    for (i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-document.addEventListener('submit',function(e){
-    e.preventDefault
-    
-})
 
+
+//This function uptdates the score section
 function setScore() {
+
+    //Updates UserName
     document.querySelector('#currentPlayer').innerText = localStorage.userName
+
+    //Updates Current Score
     currentScoreSpan.innerText = localStorage.getItem('currentScore')
+
+    //This whole spammy section retrieves data from local storage to update the game type and high score
     if(localStorage.gameType==="4"){
         document.querySelector('#currentGameType').innerText = " (4 Chicken Game)"
         document.querySelector('#highScoreGameType').innerText = " (4 Chicken Game)"
@@ -198,7 +235,18 @@ function setScore() {
 }
 
 
-//Function to assign each chicken a colored egg and put that information in local storage
+//This function automatically shuffles any array. It will be called by the 'arrangeChickensAndEggs' function to randomize placement of colored eggs.
+function shuffleArray(array) {
+    for (i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+
+//Function to create chicken and egg data in local storage. This information will be read by the 'populateGameDiv' function to create the divs and add images to get our chickens and eggs into the DOM.
 function arrangeChickensAndEggs() {
     let chickenNumber=parseInt(localStorage.gameType)
     let chickenArray= []
@@ -215,10 +263,14 @@ function arrangeChickensAndEggs() {
     }
 }
 
+
+//This function calls data from local storage to set up the UI for the game
 function populateGameDiv() {
     
-    //Clear out any junk just in case.
+    //Removes anything left in the DOM from previous games just in case.
     depopulateGameDiv()
+
+    //Updates current score.
     setScore()
 
     //Gets all our chickens out of storage and places them into the game div
@@ -258,6 +310,8 @@ function populateGameDiv() {
 
 }
 
+
+//This function removes everything from the game UI
 function depopulateGameDiv() {
     const game = document.querySelector("#gameDiv")
     while(game.firstChild) {
@@ -265,38 +319,12 @@ function depopulateGameDiv() {
     }
 }
 
-function removeEggDiv(selectedChicken) {
-    //makes sure the egg doesnt linger 
-    document.querySelector("#egg"+selectedChicken).style.transitionDuration = "0s";
-
-    //hides entire eggDiv until game is ended
-    let removalDiv = document.querySelector('#eggDiv'+ selectedChicken)
-    removalDiv.classList.add("hidden")
-
-    //Tells local storage this pair was found so we can hide them if we 'continue game' or refresh the page
-    if(localStorage.hiddenDivs===undefined) {
-        localStorage.hiddenDivs = ''
-        localStorage.hiddenDivs = localStorage.hiddenDivs + `${selectedChicken}`
-    }
-    else {
-        localStorage.hiddenDivs = localStorage.hiddenDivs + `${selectedChicken}`
-    }
-}
-
-function checkEndGame() {
-    let numFound = 0
-    
-    for(i=1;i<=parseInt(localStorage.gameType);i++){
-        if(localStorage.hiddenDivs!= undefined && localStorage.hiddenDivs.includes(`${i}`)){
-            numFound++
-        }
-    }
-    if(numFound===parseInt(localStorage.gameType)){
-        endGame()
-    }
-}
 
 
+
+
+
+//This event listener briefly hides a still image of a chicken which has been placed on top of an identically sized gif of that same chicken flapping. The intent is to make it look like your mouse has disturbed the chicken.
 document.addEventListener('mouseover',function(e) {
     if(e.target.classList.contains('chicken')){
     e.target.classList.toggle('hidden')
@@ -305,22 +333,9 @@ document.addEventListener('mouseover',function(e) {
 })
 
 
-function dropEgg(selectedChicken) {
-    document.querySelector("#egg"+selectedChicken).classList.toggle("hidden")
-    document.querySelector("#egg"+selectedChicken).style.transitionDuration = "1s";
-    document.querySelector("#egg"+selectedChicken).style.marginTop = '120%'
-    document.querySelector("#egg"+selectedChicken).style.zIndex = '40'
-}
 
-function returnEgg(selectedChicken) {
-    document.querySelector("#egg"+selectedChicken).style.transitionDuration = "0s";
-    document.querySelector("#egg"+selectedChicken).classList.toggle("hidden")
-    document.querySelector("#egg"+selectedChicken).style.marginTop = '20%'
-    document.querySelector("#egg"+selectedChicken).style.zIndex = '1'
-  
-}
 
-//Event Listener for clicking chickens
+//This event listener manages the clicking of chickens. This event listener and its supporting functions effectively manage all gameplay
 document.addEventListener('click',function(e) {
     if(e.target.classList.contains('chicken') || e.target.classList.contains('chicken2')){
         if(parseInt(localStorage.getItem('chickensSelected'))===0){
@@ -342,7 +357,7 @@ document.addEventListener('click',function(e) {
             //drop egg action
             dropEgg(localStorage.getItem('firstSelectedChicken'))
         }
-        else if(parseInt(localStorage.getItem('chickensSelected'))===1 && firstSelectedChicken != e.target.id[e.target.id.length-1]){
+        else if(parseInt(localStorage.getItem('chickensSelected'))===1 && localStorage.getItem('firstSelectedChicken') != e.target.id[e.target.id.length-1]){
             
             //tells us we have selected the first chicken and lets us know which one it is.
             localStorage.setItem('chickensSelected',99)
@@ -365,6 +380,8 @@ document.addEventListener('click',function(e) {
                 //specifies eggDivs and then sends them to the removal function
                 setTimeout(removeEggDiv,2000,localStorage.getItem('firstSelectedChicken'))
                 setTimeout(removeEggDiv,2000,localStorage.getItem('secondSelectedChicken'))
+
+                //Checks to see if all pairs have been found.
                 setTimeout(checkEndGame,2001)
                 
                 //resets the selected chickens, set to take just a little longer than the removal function so that those are all gone befor the user can select the next chicken
@@ -389,6 +406,53 @@ document.addEventListener('click',function(e) {
     }
 })
 
+//This function is called when the user selects a chicken. It gives the appearance of the chicken laying the egg and it falling into the nest below.
+function dropEgg(selectedChicken) {
+    document.querySelector("#egg"+selectedChicken).classList.toggle("hidden")
+    document.querySelector("#egg"+selectedChicken).style.transitionDuration = "1s";
+    document.querySelector("#egg"+selectedChicken).style.marginTop = '120%'
+    document.querySelector("#egg"+selectedChicken).style.zIndex = '40'
+}
+
+
+//This egg essentially resets the values changes by the 'dropEgg' function. It intentionally does this in a way that makes the egg disappear from the nest instead of rising back to the chicken.
+function returnEgg(selectedChicken) {
+    document.querySelector("#egg"+selectedChicken).style.transitionDuration = "0s";
+    document.querySelector("#egg"+selectedChicken).classList.toggle("hidden")
+    document.querySelector("#egg"+selectedChicken).style.marginTop = '20%'
+    document.querySelector("#egg"+selectedChicken).style.zIndex = '1'
+  
+}
+
+
+//This function hides matched pairs from UI
+function removeEggDiv(selectedChicken) {
+    //makes sure the egg doesnt linger 
+    document.querySelector("#egg"+selectedChicken).style.transitionDuration = "0s";
+
+    //hides entire eggDiv until game is ended
+    let removalDiv = document.querySelector('#eggDiv'+ selectedChicken)
+    removalDiv.classList.add("hidden")
+
+    //Tells local storage this pair was found so we can hide them if we 'continue game' or refresh the page
+    if(localStorage.hiddenDivs===undefined) {
+        localStorage.hiddenDivs = ''
+        localStorage.hiddenDivs = localStorage.hiddenDivs + `${selectedChicken}`
+    }
+    else {
+        localStorage.hiddenDivs = localStorage.hiddenDivs + `${selectedChicken}`
+    }
+}
+
+//This function checks to see if all pairs have been found. If they have, it calls the 'endgame' function.
+function checkEndGame() {
+
+    if(localStorage.hiddenDivs.length === parseInt(localStorage.gameType)){
+        endGame()
+    }
+}
+
+//This function clears localstorage of everything but high score information and leaves a cute note to the victorious user.
 function endGame() {
 //Store High Score if necessary
     const type = localStorage.gameType
@@ -409,10 +473,30 @@ function endGame() {
         localStorage.setItem('thirtyTwoGameHighScore',score)
         localStorage.setItem('thirtyTwoGameHighScoreUser',localStorage.userName)
     }
-    //Gets rid of all the hidden eggDivs
+    
+    //Updates score section, specifically giving the user the opportunity to see their high score reflected if they achieved it
     setScore()
+    //Gets rid of all hidden egg divs
     depopulateGameDiv()
-    //Leaves a congrats message and puts a picture of an omelette
+    //Leaves a happy note for the victor
+    congratsMessage()
+
+    //Clears out all game specific information from local storage
+    for(i=1;i<=parseInt(localStorage.gameType);i++) {
+        localStorage.removeItem(`${i}`)
+    }
+    localStorage.removeItem('currentScore')
+    localStorage.removeItem('gameType')
+    localStorage.removeItem('userName')
+    localStorage.removeItem('gameInProgress')
+    localStorage.removeItem('hiddenDivs')
+    setTimeout(function() {localStorage.removeItem('chickensSelected')},2003)
+    localStorage.removeItem('firstSelectedChicken')
+    localStorage.removeItem('secondSelectedChicken')
+}
+
+//This function puts a message and gif in the emptied game div to congratulate the user
+function congratsMessage() {
     const congrats = document.createElement('h3')
     congrats.innerText = "Congratulations, now make me an omelette!"
     congrats.style.width = "100%"
@@ -423,18 +507,4 @@ function endGame() {
     omelette.src = "omelette.gif"
     omelette.classList.add("omelette")
     document.querySelector("#gameDiv").appendChild(omelette)
-
-    //Clears out all game specific information
-    for(i=1;i<=parseInt(localStorage.gameType);i++) {
-        localStorage.removeItem(`${i}`)
-    }
-        localStorage.removeItem('currentScore')
-        localStorage.removeItem('gameType')
-        localStorage.removeItem('userName')
-        localStorage.removeItem('gameInProgress')
-        localStorage.removeItem('hiddenDivs')
-        setTimeout(function() {localStorage.removeItem('chickensSelected')},2003)
-        localStorage.removeItem('firstSelectedChicken')
-        localStorage.removeItem('secondSelectedChicken')
 }
-
